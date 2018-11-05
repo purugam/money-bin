92d5470034e0e082cee3a95db7b74379937168c0
Jasper <purugam@gmail.com>
2018-11-05 16:58:51 +0100

    Beispiel-Ausgaben hinzugefügt (expenses.csv); exp-mgr.groovy kann Ausgaben formatiert auflisten (list)

--- exp-mgr.groovy
+++ exp-mgr.groovy
@@ -1,3 +1,34 @@
-print "Press a key: "
-def c = new BufferedReader(new InputStreamReader(System.in)).readLine()
-println "You pressed $c"
+def readExpenses =
+{
+  def expFile = new File("expenses.csv")
+  def expReader = new BufferedReader(new FileReader(expFile))
+  def expLine = expReader.readLine()
+  def expenses = []
+  while(expLine)
+  {
+    expenses.add(Float.parseFloat(expLine))
+    expLine = expReader.readLine()
+  }
+  return expenses
+}
+
+def listExpenses =
+{ expenses ->
+  def max = expenses.max()
+  def format = java.text.NumberFormat.getCurrencyInstance()
+  def maxRep = format.format(max)
+  int len = maxRep.length()
+  expenses.each { println(String.format("%${len}s".toString(), format.format(it))) }
+}
+
+def expenses = readExpenses()
+def reader = new BufferedReader(new InputStreamReader(System.in))
+def line = "start"
+while(line && line != "exit")
+{
+  print "> "
+  line = reader.readLine()
+  if(line == "list")
+    listExpenses(expenses)
+}
+reader.close()
